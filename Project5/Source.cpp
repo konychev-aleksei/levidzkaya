667ea34a9c9c654c281ec832bcd1c539ebeb8341
konychev-aleksei <91011728+konychev-aleksei@users.noreply.github.com>
2021-12-02 22:20:54 +0400

    Update Source.cpp

--- Project5/Source.cpp
+++ Project5/Source.cpp
@@ -1,24 +1,27 @@
-#include "interpolation.h"
+//#include "interpolation.h"
+#include "rastr.h"
+//#include "otsech.h"
 
 RECT rct;
 
-void canvas(HDC dc, HWND hwnd) {
+void canvas(HDC dc, HWND hwnd, int x1, int y1, int x2, int y2) {
 	HDC memDC = CreateCompatibleDC(dc);
 	HBITMAP memBM = CreateCompatibleBitmap(dc, rct.right - rct.left, rct.bottom - rct.top);
-	SelectObject(memDC, memBM);
+	SelectObject(memDC, memBM); 
+	//draw(memDC, rct, x1, y1, x2, y2); 
 	draw(memDC, rct);
 	BitBlt(dc, 0, 0, rct.right - rct.left, rct.bottom - rct.top, memDC, 0, 0, SRCCOPY);
 	ReleaseDC(hwnd, memDC);
-	DeleteDC(memDC);
+	DeleteDC(memDC); 
 	DeleteObject(memBM);
 }
 
 LRESULT WINAPI WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam) {
 	if (message == WM_DESTROY)
 		PostQuitMessage(0);
-	else if (message == WM_SIZE) 
+	else if (message == WM_SIZE)
 		GetClientRect(hwnd, &rct);
-	else 
+	else
 		return DefWindowProcA(hwnd, message, wparam, lparam);
 }
 
@@ -30,25 +33,44 @@ int main() {
 	RegisterClassA(&wcl);
 
 	HWND hwnd;
-	hwnd = CreateWindow("my Window", "Lab1", WS_OVERLAPPEDWINDOW, 100, 100, 992, 515, NULL, NULL, NULL, NULL);
+	hwnd = CreateWindow("my Window", "Window", WS_OVERLAPPEDWINDOW, 100, 100, 992, SCREEN_HEIGHT, NULL, NULL, NULL, NULL);
 
 	HDC dc = GetDC(hwnd);
 	ShowWindow(hwnd, SW_SHOWNORMAL);
 
+	srand(time(NULL));
 	MSG msg;
+
+	int timer = 0;
+	int x1, y1, x2, y2;
+
 	while (1) {
+		if (timer % 120 == 0) {
+			
+			x1 = rand() % 90;
+			y1 = rand() % 50;
+			x2 = rand() % 90;
+			y2 = rand() % 50;
+			
+			
+			//x1 = rand() % 25;
+			//y1 = rand() % 25;
+			//x2 = rand() % 25 + x1 + 10;
+			//y2 = rand() % 25 + y1 + 10;
+			
+			timer = 1;
+		}
 		if (PeekMessageA(&msg, NULL, 0, 0, PM_REMOVE)) {
 			if (msg.message == WM_QUIT) break;
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
 		else {
-			canvas(dc, hwnd);
+			canvas(dc, hwnd, x1, y1, x2, y2);
 			Sleep(1);
+			timer++;
 		}
 	}
 
 
 }
-
-
